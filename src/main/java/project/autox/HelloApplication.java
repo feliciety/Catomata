951607f3d0bffae632281aeadfba51e6ce7e5205
Fe Anne Malasarte <feannemlsrte@gmail.com>
2024-09-28 17:06:41 +0800

    working code

--- src/main/java/project/autox/HelloApplication.java
+++ src/main/java/project/autox/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/project/autox/FXML/DFA.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/project/autox/FXML/Hpage.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
             stage.setTitle("AutoX");
             stage.setScene(scene);
