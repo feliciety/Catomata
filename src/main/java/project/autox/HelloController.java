b38a185773e9b49c4b675b7fac7be3b7f102b95b
Fe Anne Malasarte <feannemlsrte@gmail.com>
2024-09-28 17:54:22 +0800

    working code

--- src/main/java/project/autox/HelloController.java
+++ src/main/java/project/autox/HelloController.java
@@ -1,14 +1,42 @@
 package project.autox;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Pane contentPane;
+
+    @FXML
+    public void loadDFA() throws IOException {
+        Pane dfaPane = FXMLLoader.load(getClass().getResource("/project/autox/FXML/DFA.fxml"));
+        contentPane.getChildren().setAll(dfaPane);
+    }
+
+    @FXML
+    public void loadNFA() throws IOException {
+        Pane nfaPane = FXMLLoader.load(getClass().getResource("/project/autox/FXML/NFA.fxml"));
+        contentPane.getChildren().setAll(nfaPane);
+    }
+
+    @FXML
+    public void loadCFG() throws IOException {
+        Pane cfgPane = FXMLLoader.load(getClass().getResource("/project/autox/FXML/CFG.fxml"));
+        contentPane.getChildren().setAll(cfgPane);
+    }
+
+    @FXML
+    public void loadPDA() throws IOException {
+        Pane pdaPane = FXMLLoader.load(getClass().getResource("/project/autox/FXML/PDA.fxml"));
+        contentPane.getChildren().setAll(pdaPane);
+    }
+
+      @FXML
+    public void loadTM() throws IOException {
+        Pane tmPane = FXMLLoader.load(getClass().getResource("/project/autox/FXML/TuringMachine.fxml"));
+        contentPane.getChildren().setAll(tmPane);
     }
 }
